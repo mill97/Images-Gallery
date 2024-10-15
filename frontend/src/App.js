c57b66ca5056108ee657df7a287f928bdf70ed70
Milos Bhat <milosbhat@gmail.com>
2024-10-15 20:59:26 +0200

    First API Call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,33 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 
+
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY; 
+
 const App = () => {
   const [word, setWord] = useState("");
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-  };
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY} `)
+      .then( (res) => res.json())
+      .then( (data) => {
+          console.log(data); 
+        })
+          .catch((err) => {
+             console.log(err); 
+            })
+
+    } //Handle Search 
+  
   return (
     <div>
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
-  );
-};
+  )
+} // App 
+
 
 export default App;
