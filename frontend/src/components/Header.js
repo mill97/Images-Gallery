78a8ced26e1bbbecdd852430337f99df35114d45
Milos Bhat <milosbhat@gmail.com>
2024-10-19 10:49:36 +0200

    added SVG Logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
-
+import { ReactComponent as Logo } from '../images/logo.svg';
 const navbarStyle = {
   backgroundColor: 'lightblue',
 };
@@ -9,7 +9,7 @@ const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Navbar.Brand href="/">{title}</Navbar.Brand>
+        <Logo style={{ maxWidth: '10rem', maxHeight: '2rem' }} />
       </Container>
     </Navbar>
   );
